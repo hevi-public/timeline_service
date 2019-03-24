048c75f257e598188fe312ae6894b12e2d504e4c
Hevi <hevi.public@gmail.com>
2019-03-24 21:03:32 +0000

    update ticket.js

--- ticket.js
+++ ticket.js
@@ -1,1 +1,119 @@
-db.getCollection('ticket').find({})
+/* 1 */
+{
+    "_id" : 12,
+    "ticketNumber" : "TICKET-137",
+    "title" : "Add new functionality to existing service",
+    "description" : "",
+    "type" : "STORY",
+    "priority" : "P3",
+    "status" : "Testing",
+    "assignee" : "Bugs Bunny",
+    "reporter" : "Donald Duck",
+    "comments" : [],
+    "attachments" : [],
+    "size" : "M",
+    "start" : "2019-03-06",
+    "end" : "2019-03-18",
+    "updatedAt" : "2019-03-24",
+    "_class" : "hu.hevi.timeline.api.model.Ticket"
+}
+
+/* 2 */
+{
+    "_id" : 11,
+    "ticketNumber" : "TICKET-127",
+    "title" : "Remove old functionality from existing service",
+    "description" : "",
+    "type" : "STORY",
+    "priority" : "P2",
+    "status" : "In development",
+    "assignee" : "Marvin the Martian",
+    "reporter" : "Donald Duck",
+    "comments" : [],
+    "attachments" : [],
+    "size" : "L",
+    "start" : "2019-02-26",
+    "end" : "2019-03-10",
+    "updatedAt" : "2019-03-22",
+    "_class" : "hu.hevi.timeline.api.model.Ticket"
+}
+
+/* 3 */
+{
+    "_id" : 10,
+    "ticketNumber" : "TICKET-123",
+    "description" : "",
+    "title" : "Fix bug in existing service",
+    "type" : "BUG",
+    "priority" : "P3",
+    "status" : "Ready to play",
+    "assignee" : "Buzz Lightyear",
+    "reporter" : "Woody",
+    "comments" : [],
+    "attachments" : [],
+    "size" : "S",
+    "start" : "2019-03-12",
+    "end" : "2019-03-21",
+    "updatedAt" : "2019-03-23",
+    "_class" : "hu.hevi.timeline.api.model.Ticket"
+}
+
+/* 4 */
+{
+    "_id" : 13,
+    "ticketNumber" : "TICKET-151",
+    "title" : "Add brand new functionality to existing service",
+    "description" : "",
+    "type" : "STORY",
+    "priority" : "P2",
+    "status" : "Testing",
+    "assignee" : "Bugs Bunny",
+    "reporter" : "Donald Duck",
+    "comments" : [],
+    "attachments" : [],
+    "size" : "M",
+    "start" : "2019-03-07",
+    "end" : "2019-03-20",
+    "updatedAt" : "2019-03-21",
+    "_class" : "hu.hevi.timeline.api.model.Ticket"
+}
+
+/* 5 */
+{
+    "_id" : 14,
+    "ticketNumber" : "TICKET-111",
+    "title" : "Removed functionality",
+    "description" : "",
+    "type" : "BUG",
+    "priority" : "P1",
+    "status" : "Testing",
+    "assignee" : "Bugs Bunny",
+    "reporter" : "Donald Duck",
+    "comments" : [],
+    "attachments" : [],
+    "size" : "M",
+    "start" : "2019-03-01",
+    "end" : "2019-03-03",
+    "updatedAt" : "2019-03-15",
+    "_class" : "hu.hevi.timeline.api.model.Ticket"
+}
+
+/* 6 */
+{
+    "_id" : 15,
+    "ticketNumber" : "TICKET-121",
+    "title" : "Delete existing service",
+    "description" : "",
+    "type" : "TASK",
+    "priority" : "P3",
+    "status" : "Testing",
+    "assignee" : "Bugs Bunny",
+    "reporter" : "Donald Duck",
+    "comments" : [],
+    "attachments" : [],
+    "size" : "M",
+    "start" : "2019-03-08",
+    "end" : "2019-03-15",
+    "updatedAt" : "2019-03-24",
+    "_class" : "hu.hevi.timeline.api.model.Ticket"
+}
